d7a1a474980591e8cac8816b2232ffdb13989a1a
resherra <redouancherrat@gmail.com>
2024-02-27 00:41:12 +0100

    preprod

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="overflow-x-hidden">
+        <div className="circle-pink h-screen w-screen lg:top-[-40%] lg:left-[-15%;] md:left-[-20%;] sm:top-[-50%] sm:left-[-25%] xs:top-[-50%] xs:right-[40%]" />
+        <div className="circle-yellow h-screen w-screen xl:top-[110%] left-[80%] sm:top-[180%]" />
+        <Navbar />
+        <main className="relative overflow-hidden">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const config = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "node_modules/flowbite-react/lib/esm/**/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
+      borderRadius: {
+        "5xl": "40px",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+}
+
+export default config
